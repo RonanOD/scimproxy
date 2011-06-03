5c34760a51ae305cdb12a85ddaa0e1011c092f50
samuel@erdtman.se <samuel@erdtman.se@34e1432f-1fb2-72fa-2d63-1a37ba64b630>
2011-06-03 13:00:31 +0000

    New complex type classes

---
+++ ScimCore/src/info/simplecloud/core/Name.java
@@ -0,0 +1,12 @@
+package info.simplecloud.core;
+
+public class Name extends ComplexType {
+    public static final String ATTRIBUTE_FORMATTED        = "formatted";
+    public static final String ATTRIBUTE_FAMILY_NAME      = "familyName";
+    public static final String ATTRIBUTE_GIVEN_NAME       = "givenName";
+    public static final String ATTRIBUTE_MIDDLE_NAME      = "middleName";
+    public static final String ATTRIBUTE_HONORIFIC_PREFIX = "honorificPrefix";
+    public static final String ATTRIBUTE_HONORIFIC_SUFFIX = "honorificSuffix";
+    public static final String simple[]                   = { ATTRIBUTE_FORMATTED, ATTRIBUTE_FAMILY_NAME, ATTRIBUTE_GIVEN_NAME,
+            ATTRIBUTE_MIDDLE_NAME, ATTRIBUTE_HONORIFIC_PREFIX, ATTRIBUTE_HONORIFIC_SUFFIX };
+}
