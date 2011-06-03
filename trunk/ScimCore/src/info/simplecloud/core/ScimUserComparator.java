f3221fe9f2d61728ca8f5a10646b0df35910bbbc
samuel@erdtman.se <samuel@erdtman.se@34e1432f-1fb2-72fa-2d63-1a37ba64b630>
2011-06-03 13:03:57 +0000

    introduced generics

--- trunk/ScimCore/src/info/simplecloud/core/ScimUserComparator.java
+++ trunk/ScimCore/src/info/simplecloud/core/ScimUserComparator.java
@@ -1,10 +1,8 @@
 package info.simplecloud.core;
 
-import info.simplecloud.core.execeptions.UnknownAttribute;
-
 import java.util.Comparator;
 
-public class ScimUserComparator implements Comparator<ScimUser> {
+public class ScimUserComparator<T> implements Comparator<ScimUser> {
 
     private String  compareAttribute;
     private boolean sortAscending;
@@ -16,14 +14,9 @@ public class ScimUserComparator implements Comparator<ScimUser> {
 
     @Override
     public int compare(ScimUser user1, ScimUser user2) {
-        try {
-            Comparable obj1 = user1.getRaw(this.compareAttribute);
-            Comparable obj2 = user2.getRaw(this.compareAttribute);
-            return this.sortAscending ? obj1.compareTo(obj2) : obj2.compareTo(obj1);
-        } catch (UnknownAttribute e) {
-            throw new RuntimeException(e);
-        }
-
+        Comparable<T> obj1 = user1.getComparable(this.compareAttribute);
+        Comparable<T> obj2 = user2.getComparable(this.compareAttribute);
+        return this.sortAscending ? obj1.compareTo((T) obj2) : obj2.compareTo((T) obj1);
     }
 
 }
