ded25cb8206803f7432728abbe67f4d3af762e9e
samuel@erdtman.se <samuel@erdtman.se@34e1432f-1fb2-72fa-2d63-1a37ba64b630>
2011-06-03 13:02:55 +0000

    Improved the complex type test

--- ScimCore/test/info/simplecloud/core/ComplexTypeTest.java
+++ ScimCore/test/info/simplecloud/core/ComplexTypeTest.java
@@ -10,8 +10,21 @@ public class ComplexTypeTest {
     @Test
     public void getAttribute() throws UnknownAttribute {
         Object test = new Object();
-        ComplexType ct = new ComplexType().addAttribute("test", test);
+        ComplexType ct = new ComplexType();
+        ct.setAttribute("test1", "Hello")
+                .setAttribute("test2", test)
+                .setAttribute("test3", test)
+                .setAttribute(
+                        "parent",
+                        new ComplexType().setAttribute("child",
+                                new ComplexType().setAttribute("grandchild1", "World").setAttribute("grandchild2", "G2")));
 
-        Assert.assertEquals(test, ct.getAttribute("test"));
+        Assert.assertNotNull(ct.getAttribute("parent"));
+        Assert.assertNotNull(ct.getAttribute("parent.child"));
+        Assert.assertEquals("World", ct.getAttribute("parent.child.grandchild1"));
+        Assert.assertEquals("G2", ct.getAttribute("parent.child.grandchild2"));
+        Assert.assertEquals("Hello", ct.getAttribute("test1"));
+        Assert.assertNull(ct.getAttribute("unknown"));
+        Assert.assertNull(ct.getAttribute("parent.child.unknown"));
     }
 }
