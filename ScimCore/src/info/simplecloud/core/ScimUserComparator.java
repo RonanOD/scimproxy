ae72c8b2d1f18a6f1eeea944bda3105a1e09bdbd
samuel@erdtman.se <samuel@erdtman.se@34e1432f-1fb2-72fa-2d63-1a37ba64b630>
2011-06-03 09:15:54 +0000

    base class update, small steps

--- ScimCore/src/info/simplecloud/core/ScimUserComparator.java
+++ ScimCore/src/info/simplecloud/core/ScimUserComparator.java
@@ -1,23 +1,29 @@
 package info.simplecloud.core;
 
+import info.simplecloud.core.execeptions.UnknownAttribute;
+
 import java.util.Comparator;
 
 public class ScimUserComparator implements Comparator<ScimUser> {
 
-    private String compareAttribute;
+    private String  compareAttribute;
     private boolean sortAscending;
 
     public ScimUserComparator(String compareAttribute, boolean sortAscending) {
         this.compareAttribute = compareAttribute;
         this.sortAscending = sortAscending;
     }
-    
+
     @Override
     public int compare(ScimUser user1, ScimUser user2) {
-        Comparable obj1 = user1.getRaw(this.compareAttribute);
-        Comparable obj2 = user2.getRaw(this.compareAttribute);
+        try {
+            Comparable obj1 = user1.getRaw(this.compareAttribute);
+            Comparable obj2 = user2.getRaw(this.compareAttribute);
+            return this.sortAscending ? obj1.compareTo(obj2) : obj2.compareTo(obj1);
+        } catch (UnknownAttribute e) {
+            throw new RuntimeException(e);
+        }
 
-        return this.sortAscending ? obj1.compareTo(obj2) : obj2.compareTo(obj1);
     }
 
 }
