5fabddd9b6c6a72e52addcfaeaa1b3393d9a7d60
erik@wahlstromstekniska.se <erik@wahlstromstekniska.se@34e1432f-1fb2-72fa-2d63-1a37ba64b630>
2011-06-06 12:46:52 +0000

    Returning real SCIM users from dummy storage.

--- scimproxy/src/main/java/info/simplecloud/scimproxy/ScimUsersServlet.java
+++ scimproxy/src/main/java/info/simplecloud/scimproxy/ScimUsersServlet.java
@@ -1,6 +1,12 @@
 package info.simplecloud.scimproxy;
 
+import info.simplecloud.core.ScimUser;
+import info.simplecloud.core.encoding.JsonEncoder;
+import info.simplecloud.core.execeptions.EncodingFailed;
+import info.simplecloud.scimproxy.storage.dummy.DummyStorage;
+
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.logging.Logger;
 
 import javax.servlet.http.HttpServletRequest;
@@ -15,49 +21,37 @@ curl -i -H "Accept: application/json" http://localhost:8080/Users
 
 @SuppressWarnings("serial")
 public class ScimUsersServlet extends Authenticated {
-	
-	private static final Logger log = Logger.getLogger(ScimUsersServlet.class.getName());
 
-	
-	public void doGet(HttpServletRequest req, HttpServletResponse resp)
-			throws IOException {
-		
-		log.warning("Hardcoded response!");
-		
-		resp.setContentType(CONTENT_TYPE_JSON);
-		resp.getWriter().println(
-			"{\n" +
-				"\t\"totalResults\": 3,\n" +
-				"\t\"entry\": [\n" +
-					"\t\t{\n" +
-						"\t\t\t\"schemas\": [\"urn:scim:schemas:core:1.0\"],\n" +
-						"\t\t\t\"id\": \"3f62ce30-dcd6-4dd7-abfe-2352a76f9978\",\n" +
-						"\t\t\t\"displayName\": \"Babs Jensen\",\n" +
-						"\t\t\t\"userName\": \"bjensen\",\n" +
-						"\t\t\t\"meta\": {\n" +
-							"\t\t\t\t\"location\":\"http://example.com/User/3f62ce30-...\"\n" +
-						"\t\t\t}\n" +
-					"\t\t},\n" +
-					"\t\t{\n" +
-						"\t\t\t\"schemas\": [\"urn:scim:schemas:core:1.0\"],\n" +
-						"\t\t\t\"id\": \"4f62ce30-dcd6-4dd7-abfe-2352a76f9978\",\n" +
-						"\t\t\t\"displayName\": \"Mandy Pepperidge\",\n" +
-						"\t\t\t\"userName\": \"mPepperidge\",\n" +
-						"\t\t\t\"meta\": {\n" +
-							"\t\t\t\t\"location\":\"http://example.com/User/4f62ce30-...\"\n" +
-						"\t\t\t}\n" +
-					"\t\t},\n" +
-					"\t\t{\n" +
-						"\t\t\t\"schemas\": [\"urn:scim:schemas:core:1.0\"],\n" +
-						"\t\t\t\"id\": \"5f62ce30-dcd6-4dd7-abfe-2352a76f9978\",\n" +
-						"\t\t\t\"displayName\": \"Noel Smith\",\n" +
-						"\t\t\t\"userName\": \"nsmith\",\n" +
-						"\t\t\t\"meta\": {\n" +
-							"\t\t\t\t\"location\":\"http://example.com/User/5f62ce30-...\"\n" +
-						"\t\t\t}\n" +
-					"\t\t}\n" +
-				"\t]\n" +
-			"}\n");
-		resp.setStatus(HttpServletResponse.SC_OK);
+	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+
+		DummyStorage storage = DummyStorage.getInstance();
+		ArrayList<ScimUser> users = storage.getList();
+
+		// TODO: SPEC: REST: Really missing an XML represenation of two or more
+		// nodes in List/Query a response.
+		try {
+
+			String response = "";
+
+			response = "{\n" + "\t\"totalResults\": " + users.size() + ",\n" + "\t\"entry\": [\n";
+
+			// TODO: Should this be done in core? Return the JSON list of more resporces when you send an List into encode method?
+			for(int i=0; i<users.size(); i++) {
+				ScimUser scimUser = users.get(i);
+				if(i != 0) {
+					response += ",";
+				}
+				response += new JsonEncoder().encode(scimUser) + "\n";
+			}
+
+			response += "\t]\n" + "}\n";
+
+			resp.setContentType(CONTENT_TYPE_JSON);
+			resp.getWriter().print(response);
+			resp.setStatus(HttpServletResponse.SC_OK);
+		} catch (EncodingFailed e) {
+			e.printStackTrace();
+			resp.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+		}
 	}
 }
