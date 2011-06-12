2829aa4df62356086e26f0a8dad756ed8f9b2448
erik@wahlstromstekniska.se <erik@wahlstromstekniska.se@34e1432f-1fb2-72fa-2d63-1a37ba64b630>
2011-06-12 15:10:01 +0000

    Adding ETag, location and attributes to meta.

--- ScimCore/src/main/java/info/simplecloud/core/Meta.java
+++ ScimCore/src/main/java/info/simplecloud/core/Meta.java
@@ -5,7 +5,10 @@ import java.util.Calendar;
 public class Meta extends ComplexType {
     public static final String ATTRIBUTE_CREATED       = "created";
     public static final String ATTRIBUTE_LAST_MODIFIED = "lastModified";
-    public static final String simple[]                = { ATTRIBUTE_CREATED, ATTRIBUTE_LAST_MODIFIED };
+    public static final String ATTRIBUTE_ETAG = "ETag";
+    public static final String ATTRIBUTE_LOCATION = "location";
+    public static final String ATTRIBUTE_ATTRIBUTE = "attributes";
+    public static final String simple[]                = { ATTRIBUTE_CREATED, ATTRIBUTE_LAST_MODIFIED, ATTRIBUTE_ETAG, ATTRIBUTE_LOCATION, ATTRIBUTE_ATTRIBUTE};
 
     public Calendar getCreated() {
         return super.getAttributeCalendar(ATTRIBUTE_CREATED);
@@ -15,12 +18,37 @@ public class Meta extends ComplexType {
         return super.getAttributeCalendar(ATTRIBUTE_LAST_MODIFIED);
     }
 
+    public String getETag() {
+        return super.getAttributeString(ATTRIBUTE_ETAG);
+    }
+
+    public String getLocation() {
+        return super.getAttributeString(ATTRIBUTE_LOCATION);
+    }
+
+    public Object getAttributes() {
+        return super.getAttribute(ATTRIBUTE_ATTRIBUTE);
+    }
+
+    
     public void setCreated(Calendar created) {
         super.setAttribute(ATTRIBUTE_CREATED, created);
     }
 
     public void setLastModified(Calendar lastModified) {
         super.setAttribute(ATTRIBUTE_LAST_MODIFIED, lastModified);
+    }
+
+    public void setETag(String etag) {
+        super.setAttribute(ATTRIBUTE_ETAG, etag);
+    }
+
+    public void setLocation(String location) {
+        super.setAttribute(ATTRIBUTE_LOCATION, location);
+    }
+    
+    public void setAttributes(Object attribute) {
+    	super.setAttribute(ATTRIBUTE_ATTRIBUTE, attribute);
     }
 
     @Override
