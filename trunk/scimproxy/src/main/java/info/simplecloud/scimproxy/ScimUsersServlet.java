940cfffd36a665e64dfd4d848ba76df1ac245b6d
samuel@erdtman.se <samuel@erdtman.se@34e1432f-1fb2-72fa-2d63-1a37ba64b630>
2011-06-14 21:08:39 +0000

    Attributes and sort parameters are used

--- trunk/scimproxy/src/main/java/info/simplecloud/scimproxy/ScimUsersServlet.java
+++ trunk/scimproxy/src/main/java/info/simplecloud/scimproxy/ScimUsersServlet.java
@@ -1,44 +1,72 @@
 package info.simplecloud.scimproxy;
 
 import info.simplecloud.core.ScimUser;
+import info.simplecloud.core.ScimUserComparator;
 import info.simplecloud.core.encoding.JsonEncoder;
 import info.simplecloud.core.execeptions.EncodingFailed;
 import info.simplecloud.scimproxy.storage.dummy.DummyStorage;
 
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.logging.Logger;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.swing.plaf.ListUI;
+
+import org.apache.commons.lang.ArrayUtils;
 
 /*
 
-GET:
-curl -i -H "Accept: application/json" http://localhost:8080/Users  
+ GET:
+ curl -i -H "Accept: application/json" http://localhost:8080/Users  
 
  */
 
 @SuppressWarnings("serial")
 public class ScimUsersServlet extends RestServlet {
 
-	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+    public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
 
-		DummyStorage storage = DummyStorage.getInstance();
-		ArrayList<ScimUser> users = storage.getList();
+        String attributesString = req.getParameter("attributes") == null ? "" : req.getParameter("attributes");
+        List<String> attributesList = new ArrayList<String>();
+        for(String attribute: attributesString.split(",")) {
+            attributesList.add(attribute.trim());
+        }
+        
+        // TODO use filter things
+        String filterBy = req.getParameter("filterBy");
+        String filterValue = req.getParameter("filterValue");
+        String filterOp = req.getParameter("filterOp");
 
-		// TODO: SPEC: REST: Really missing an XML representation of two or more
-		// nodes in List/Query a response.
-		try {
-			String response = "";
-			response += new JsonEncoder().encode(users);
+        // TODO: SPEC: REST: what is major
+        String sortBy = req.getParameter("sortBy") == null ? "userName" : req.getParameter("sortBy");
+        String sortOrder = req.getParameter("sortOrder") == null ? "ascending" : req.getParameter("sortOrder");
+        if (!sortOrder.equalsIgnoreCase("ascending") && !sortOrder.equalsIgnoreCase("descending")) {
+            resp.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+            resp.getWriter().print("Sort order must be 'ascending' or 'descending'");
+            return;
+        }
 
-			resp.setContentType(CONTENT_TYPE_JSON);
-			resp.getWriter().print(response);
-			resp.setStatus(HttpServletResponse.SC_OK);
-		} catch (EncodingFailed e) {
-			e.printStackTrace();
-			resp.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
-		}
-	}
+        DummyStorage storage = DummyStorage.getInstance();
+        List<ScimUser> users = storage.getList();
+        
+        Collections.sort(users, new ScimUserComparator<String>(sortBy, sortOrder.equalsIgnoreCase("ascending")));
+        
+        
+        // TODO: SPEC: REST: Really missing an XML representation of two or more
+        // nodes in List/Query a response.
+        try {
+            resp.setStatus(HttpServletResponse.SC_OK);
+            resp.setContentType(CONTENT_TYPE_JSON);
+
+            String response = new JsonEncoder().encode(users, attributesList);
+            resp.getWriter().print(response);
+        } catch (EncodingFailed e) {
+            resp.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+            resp.getWriter().print("Error: failed to build user set");
+        }
+    }
 }
