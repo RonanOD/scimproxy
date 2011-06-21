090a99609c72aa82a1cecff49dda4678769d8d16
samuel@erdtman.se <samuel@erdtman.se@34e1432f-1fb2-72fa-2d63-1a37ba64b630>
2011-06-21 19:02:47 +0000

    No real change

--- trunk/ScimCore/src/test/java/info/simplecloud/core/tools/ScimUserComparatorTest.java
+++ trunk/ScimCore/src/test/java/info/simplecloud/core/tools/ScimUserComparatorTest.java
@@ -1,10 +1,12 @@
 package info.simplecloud.core.tools;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 public class ScimUserComparatorTest {
     @Test
     public void sortCollection() {
-
+        //TODO implement
+        //Assert.fail("Not implemented");
     }
 }
