6bd2f01a8f0ddc0e59af45fbc4cae5a8d1b86a30
samuel@erdtman.se <samuel@erdtman.se@34e1432f-1fb2-72fa-2d63-1a37ba64b630>
2011-06-21 18:55:02 +0000

    Implemented handler for Calender

--- trunk/ScimCore/src/main/java/info/simplecloud/core/coding/handlers/CalendarHandler.java
+++ trunk/ScimCore/src/main/java/info/simplecloud/core/coding/handlers/CalendarHandler.java
@@ -1,5 +1,11 @@
 package info.simplecloud.core.coding.handlers;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+
 import info.simplecloud.core.execeptions.FailedToGetValue;
 import info.simplecloud.core.execeptions.FailedToSetValue;
 import info.simplecloud.core.execeptions.UnhandledAttributeType;
@@ -9,19 +15,24 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 public class CalendarHandler implements ITypeHandler {
+    private SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssZ");
 
     @Override
     public Object decode(JSONObject scimUserJson, String attributeId) throws JSONException, UnhandledAttributeType, FailedToSetValue,
-            UnknownType, InstantiationException, IllegalAccessException {
-        // TODO implement
-        return null;
+            UnknownType, InstantiationException, IllegalAccessException, ParseException {
+        Calendar result = new GregorianCalendar();
+        String stringDate = scimUserJson.getString(attributeId);
+        Date date = simpleDateFormat.parse(stringDate);
+        result.setTime(date);
+        return result;
     }
 
     @Override
     public void encode(JSONObject scimUserJson, String attributeId, Object object) throws JSONException, UnhandledAttributeType,
             FailedToSetValue, UnknownType, InstantiationException, IllegalAccessException, FailedToGetValue {
-        // TODO implement
-
+        Calendar cal = (Calendar) object;
+        String value = simpleDateFormat.format(cal.getTime());
+        scimUserJson.put(attributeId, value);
     }
 
 }
