dedd8eb72d0a299875679797e07318720f11cb14
erik@wahlstromstekniska.se <erik@wahlstromstekniska.se@34e1432f-1fb2-72fa-2d63-1a37ba64b630>
2011-06-28 15:41:00 +0000

    Adding JODA to parse xd:dateTime strings.

--- ScimCore/src/main/java/info/simplecloud/core/coding/handlers/CalendarHandler.java
+++ ScimCore/src/main/java/info/simplecloud/core/coding/handlers/CalendarHandler.java
@@ -1,28 +1,34 @@
 package info.simplecloud.core.coding.handlers;
 
+import info.simplecloud.core.execeptions.FailedToGetValue;
+import info.simplecloud.core.execeptions.FailedToSetValue;
+import info.simplecloud.core.execeptions.UnhandledAttributeType;
+import info.simplecloud.core.execeptions.UnknownType;
+
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.GregorianCalendar;
 
-import info.simplecloud.core.execeptions.FailedToGetValue;
-import info.simplecloud.core.execeptions.FailedToSetValue;
-import info.simplecloud.core.execeptions.UnhandledAttributeType;
-import info.simplecloud.core.execeptions.UnknownType;
-
+import org.joda.time.DateTime;
+import org.joda.time.format.DateTimeFormatter;
+import org.joda.time.format.ISODateTimeFormat;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 public class CalendarHandler implements ITypeHandler {
+    private static final DateTimeFormatter XML_DATE_TIME_FORMAT = ISODateTimeFormat.dateTimeNoMillis();
     private SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssZ");
 
+    
     @Override
     public Object decode(JSONObject scimUserJson, String attributeId) throws JSONException, UnhandledAttributeType, FailedToSetValue,
             UnknownType, InstantiationException, IllegalAccessException, ParseException {
         Calendar result = new GregorianCalendar();
         String stringDate = scimUserJson.getString(attributeId);
-        Date date = simpleDateFormat.parse(stringDate);
+        DateTime dt = XML_DATE_TIME_FORMAT.parseDateTime(stringDate);
+        Date date = dt.toDate();
         result.setTime(date);
         return result;
     }
