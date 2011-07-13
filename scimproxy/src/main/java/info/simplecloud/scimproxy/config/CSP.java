300fb8e981c03a3cc9509a831a3fa9b144b3c968
erik@wahlstromstekniska.se <erik@wahlstromstekniska.se@34e1432f-1fb2-72fa-2d63-1a37ba64b630>
2011-07-13 09:24:15 +0000

    Adding support for downstreams CSP using REST calls. POST only supported at the moment.

---
+++ scimproxy/src/main/java/info/simplecloud/scimproxy/config/CSP.java
@@ -0,0 +1,63 @@
+package info.simplecloud.scimproxy.config;
+
+public class CSP {
+
+	public static String AUTH_BASIC = "basic";
+	
+	private String url = "";
+	private String authentication ="";
+	private String basicUsername = "";
+	private String basicPassword = "";
+	private String preferedEncoding = "JSON";
+	
+	public CSP() {
+		
+	}
+
+	public void setUrl(String url) {
+		this.url = url;
+	}
+
+	public String getUrl() {
+		return url;
+	}
+
+	public void setAuthentication(String authentication) {
+		this.authentication = authentication;
+	}
+
+	public String getAuthentication() {
+		return authentication;
+	}
+
+	public void setBasicUsername(String basicUsername) {
+		this.basicUsername = basicUsername;
+	}
+
+	public String getBasicUsername() {
+		return basicUsername;
+	}
+
+	public void setBasicPassword(String basicPassword) {
+		this.basicPassword = basicPassword;
+	}
+
+	public String getBasicPassword() {
+		return basicPassword;
+	}
+	
+	public String toString() {
+		// don't print password
+		return "url=" + url + ", auth=" + authentication + ", basicUsername=" + basicUsername;
+	}
+
+	public void setPreferedEncoding(String preferedEncoding) {
+		this.preferedEncoding = preferedEncoding;
+	}
+
+	public String getPreferedEncoding() {
+		return preferedEncoding;
+	}
+	
+	
+}
