3486c5e87f5e4f4f9d29397a314c845d30c5ee67
samuel@erdtman.se <samuel@erdtman.se@34e1432f-1fb2-72fa-2d63-1a37ba64b630>
2011-07-14 06:17:53 +0000

    Upgraded from junit3 to junit4

--- trunk/scimproxy/src/test/java/info/simplecloud/scimproxy/ScimUserServletPutTest.java
+++ trunk/scimproxy/src/test/java/info/simplecloud/scimproxy/ScimUserServletPutTest.java
@@ -1,83 +1,84 @@
 package info.simplecloud.scimproxy;
 
 import info.simplecloud.core.ScimUser;
-import junit.framework.TestCase;
 
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
 import org.mortbay.jetty.servlet.DefaultServlet;
 import org.mortbay.jetty.testing.HttpTester;
 import org.mortbay.jetty.testing.ServletTester;
 
-public class ScimUserServletPutTest extends TestCase {
+public class ScimUserServletPutTest {
 
-	HttpTester request = new HttpTester();
-	HttpTester response = new HttpTester();
-	ServletTester tester = null;
+    HttpTester     request  = new HttpTester();
+    HttpTester     response = new HttpTester();
+    ServletTester  tester   = null;
 
-	private String id = "";
-	
-	public void setUp() throws Exception {
-		tester = new ServletTester();
-		tester.addServlet(ScimUserServlet.class, "/User/*");
-	    tester.addServlet(DefaultServlet.class, "/");
-	    tester.start();
-	    
-	    ScimUser scimUser = new ScimUser();
-	    scimUser.setUserName("Alice");
+    private String id       = "";
 
-		request.setMethod("POST");
-		request.setVersion("HTTP/1.0");
-		request.setHeader("Authorization", "Basic dXNyOnB3");
+    @BeforeClass
+    public void setUp() throws Exception {
+        tester = new ServletTester();
+        tester.addServlet(ScimUserServlet.class, "/User/*");
+        tester.addServlet(DefaultServlet.class, "/");
+        tester.start();
 
-		request.setURI("/User");
-		request.setHeader("Content-Length", Integer.toString(scimUser.getUser("JSON").length()));
-		request.setHeader("Content-Type", "application/x-www-form-urlencoded");
-		request.setContent(scimUser.getUser("JSON"));
-		response.parse(tester.getResponses(request.generate()));
+        ScimUser scimUser = new ScimUser();
+        scimUser.setUserName("Alice");
 
-		ScimUser tmp = new ScimUser(response.getContent(), "JSON");
-		id = tmp.getId();
-	}
+        request.setMethod("POST");
+        request.setVersion("HTTP/1.0");
+        request.setHeader("Authorization", "Basic dXNyOnB3");
 
+        request.setURI("/User");
+        request.setHeader("Content-Length", Integer.toString(scimUser.getUser("JSON").length()));
+        request.setHeader("Content-Type", "application/x-www-form-urlencoded");
+        request.setContent(scimUser.getUser("JSON"));
+        response.parse(tester.getResponses(request.generate()));
 
-	public void testPutUser() throws Exception {
-	    ScimUser scimUser = new ScimUser();
-	    scimUser.setId(id);
-	    scimUser.setUserName("Bob");
-	    
-		request.setMethod("PUT");
-		request.setVersion("HTTP/1.0");
+        ScimUser tmp = new ScimUser(response.getContent(), "JSON");
+        id = tmp.getId();
+    }
 
-		request.setURI("/User/" + id);
+    @Test
+    public void putUser() throws Exception {
+        ScimUser scimUser = new ScimUser();
+        scimUser.setId(id);
+        scimUser.setUserName("Bob");
 
-		request.setHeader("Content-Length", Integer.toString(scimUser.getUser("JSON").length()));
-		request.setHeader("Content-Type", "application/x-www-form-urlencoded");
-		request.setContent(scimUser.getUser("JSON"));
-		response.parse(tester.getResponses(request.generate()));
+        request.setMethod("PUT");
+        request.setVersion("HTTP/1.0");
 
-		assertEquals(200, response.getStatus());
-		        
+        request.setURI("/User/" + id);
+
+        request.setHeader("Content-Length", Integer.toString(scimUser.getUser("JSON").length()));
+        request.setHeader("Content-Type", "application/x-www-form-urlencoded");
+        request.setContent(scimUser.getUser("JSON"));
+        response.parse(tester.getResponses(request.generate()));
+
+        Assert.assertEquals(200, response.getStatus());
+
         ScimUser returnedUser = new ScimUser(response.getContent(), "JSON");
-        
-        assertEquals(id, returnedUser.getId());
-        assertEquals("Bob", returnedUser.getUserName());
-	}
-	
 
-	public void testPutInvalidUser() throws Exception {
-		request.setMethod("PUT");
-		request.setVersion("HTTP/1.0");
+        Assert.assertEquals(id, returnedUser.getId());
+        Assert.assertEquals("Bob", returnedUser.getUserName());
+    }
 
-		request.setURI("/User/" + id);
+    @Test
+    public void putInvalidUser() throws Exception {
+        request.setMethod("PUT");
+        request.setVersion("HTTP/1.0");
 
-		request.setHeader("Content-Length", Integer.toString("very invalid user".length()));
-		request.setHeader("Content-Type", "application/x-www-form-urlencoded");
-		request.setContent("very invalid user");
-		response.parse(tester.getResponses(request.generate()));
+        request.setURI("/User/" + id);
 
-		assertEquals(400, response.getStatus());
-		        
-	}
+        request.setHeader("Content-Length", Integer.toString("very invalid user".length()));
+        request.setHeader("Content-Type", "application/x-www-form-urlencoded");
+        request.setContent("very invalid user");
+        response.parse(tester.getResponses(request.generate()));
 
+        Assert.assertEquals(400, response.getStatus());
+
+    }
 
 }
-
