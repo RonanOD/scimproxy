c0bd94ca1e779e308ba8fdd2eedb66abe9f63149
erik@wahlstromstekniska.se <erik@wahlstromstekniska.se@34e1432f-1fb2-72fa-2d63-1a37ba64b630>
2011-07-20 11:41:26 +0000

    Adding auth defined in config file to Add and List.

--- scimproxy/src/main/java/info/simplecloud/scimproxy/viewer/Add.java
+++ scimproxy/src/main/java/info/simplecloud/scimproxy/viewer/Add.java
@@ -1,5 +1,7 @@
 package info.simplecloud.scimproxy.viewer;
 
+import info.simplecloud.scimproxy.authentication.Authenticator;
+import info.simplecloud.scimproxy.config.Config;
 import info.simplecloud.scimproxy.trigger.Trigger;
 
 import java.io.IOException;
@@ -25,43 +27,54 @@ public class Add extends HttpServlet {
 
 	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
 	
-		String user = req.getParameter("user");
-        resp.getWriter().print("<html>");
-        resp.getWriter().print("<a href=\"List\">List</a>&nbsp;");
-        resp.getWriter().print("<a href=\"Add\">Add user</a>");
-        resp.getWriter().print("<br/>");
-
-		if(user!= null && !"".equals(user)) {
-			// Create an instance of HttpClient.
-			HttpClient client = new HttpClient();
-
-			// set auth if it's authenticated
-			client.getParams().setAuthenticationPreemptive(true);
-			Credentials defaultcreds = new UsernamePasswordCredentials("usr", "pw");
-			client.getState().setCredentials(AuthScope.ANY, defaultcreds);
-			
-			// Create a method instance.
-			PostMethod method = new PostMethod("http://localhost:8080/User");
-			method.getParams().setParameter(HttpMethodParams.RETRY_HANDLER, new DefaultHttpMethodRetryHandler(3, false));
-	        method.setRequestBody(user);
-
-			int responseCode = client.executeMethod(method);
-			if(responseCode == 201) {
-		        resp.getWriter().print("User added successfully.<br/><br/>");
-			}
-			else {
-		        resp.getWriter().print("Failed to add user to storage.<br/><br/>");
-			}
+		Config config = null;
+		config = Config.getInstance();
+		// authenticate
+		Authenticator auth = new Authenticator(config);
+		if(!auth.authenticate(req, resp)) {
+		     resp.setHeader("WWW-authenticate", "basic  realm='mytest'");
+			resp.sendError(HttpServletResponse.SC_UNAUTHORIZED, "No matching authentication schema could be found.");
 		}
-
-        resp.getWriter().print("<br/><br/><form>");
-        resp.getWriter().print("<input type=\"radio\" name=\"encoding\" value=\"JSON\" checked=\"true\" /> JSON<br/>");
-        resp.getWriter().print("<input type=\"radio\" name=\"encoding\" disabled=\"true\" value=\"XML\" /> XML<br/>");
-        resp.getWriter().print("<textarea cols=\"50\" rows=\"15\" name=\"user\"></textarea><br/>");
-        resp.getWriter().print("<input type=\"submit\" value=\"Add user\"><br/>");
-        resp.getWriter().print("</form></html>");
-
-		resp.setStatus(HttpServletResponse.SC_OK);
+		else {
+			
+			String user = req.getParameter("user");
+	        resp.getWriter().print("<html>");
+	        resp.getWriter().print("<a href=\"List\">List</a>&nbsp;");
+	        resp.getWriter().print("<a href=\"Add\">Add user</a>");
+	        resp.getWriter().print("<br/>");
+	
+			if(user!= null && !"".equals(user)) {
+				// Create an instance of HttpClient.
+				HttpClient client = new HttpClient();
+	
+				// set auth if it's authenticated
+				client.getParams().setAuthenticationPreemptive(true);
+				Credentials defaultcreds = new UsernamePasswordCredentials("usr", "pw");
+				client.getState().setCredentials(AuthScope.ANY, defaultcreds);
+				
+				// Create a method instance.
+				PostMethod method = new PostMethod("http://localhost:8080/User");
+				method.getParams().setParameter(HttpMethodParams.RETRY_HANDLER, new DefaultHttpMethodRetryHandler(3, false));
+		        method.setRequestBody(user);
+	
+				int responseCode = client.executeMethod(method);
+				if(responseCode == 201) {
+			        resp.getWriter().print("User added successfully.<br/><br/>");
+				}
+				else {
+			        resp.getWriter().print("Failed to add user to storage.<br/><br/>");
+				}
+			}
+	
+	        resp.getWriter().print("<br/><br/><form>");
+	        resp.getWriter().print("<input type=\"radio\" name=\"encoding\" value=\"JSON\" checked=\"true\" /> JSON<br/>");
+	        resp.getWriter().print("<input type=\"radio\" name=\"encoding\" disabled=\"true\" value=\"XML\" /> XML<br/>");
+	        resp.getWriter().print("<textarea cols=\"50\" rows=\"15\" name=\"user\"></textarea><br/>");
+	        resp.getWriter().print("<input type=\"submit\" value=\"Add user\"><br/>");
+	        resp.getWriter().print("</form></html>");
+	
+			resp.setStatus(HttpServletResponse.SC_OK);
+		}
 	}
 
 }
