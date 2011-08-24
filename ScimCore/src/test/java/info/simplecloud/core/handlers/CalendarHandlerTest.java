580f2f0d6efd10e8bf472ff4375cbe9dad23d515
samuel@erdtman.se <samuel@erdtman.se@34e1432f-1fb2-72fa-2d63-1a37ba64b630>
2011-08-24 18:48:17 +0000

    Minor test changes to test simple encode decode cases for xml

---
+++ ScimCore/src/test/java/info/simplecloud/core/handlers/CalendarHandlerTest.java
@@ -0,0 +1,37 @@
+package info.simplecloud.core.handlers;
+
+import info.simplecloud.core.exceptions.InvalidUser;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.TimeZone;
+
+import org.junit.Test;
+
+public class CalendarHandlerTest {
+    private static CalendarHandler ch = new CalendarHandler();
+
+    @Test(expected = InvalidUser.class)
+    public void encodeInvalideDate() throws InvalidUser {
+        String date = "2011-12-33T22:22:22T3";
+        ch.decode(date, null, null);
+    }
+
+    @Test
+    public void encode() throws InvalidUser {
+        TimeZone tz = TimeZone.getTimeZone("GMT+00:00");
+        Calendar cal = Calendar.getInstance(tz);
+        cal.setTime(new Date(System.currentTimeMillis()));
+        ch.encode(cal, null, null);
+
+        // TODO check answer
+    }
+
+    @Test
+    public void decode() throws InvalidUser {
+        String date = "2008-01-23T04:56:22Z";
+        ch.decode(date, null, null);
+
+        // TODO check answer
+    }
+}
