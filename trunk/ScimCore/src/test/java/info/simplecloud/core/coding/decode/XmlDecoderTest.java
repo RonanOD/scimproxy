e1a5a1135dc0599f603d3e94e567ecb7f693a046
samuel@erdtman.se <samuel@erdtman.se@34e1432f-1fb2-72fa-2d63-1a37ba64b630>
2011-09-01 19:58:56 +0000

    xml and patch tests

--- trunk/ScimCore/src/test/java/info/simplecloud/core/coding/decode/XmlDecoderTest.java
+++ trunk/ScimCore/src/test/java/info/simplecloud/core/coding/decode/XmlDecoderTest.java
@@ -1,5 +1,7 @@
 package info.simplecloud.core.coding.decode;
 
+import junit.framework.Assert;
+import info.simplecloud.core.User;
 import info.simplecloud.core.exceptions.InvalidUser;
 
 import org.junit.Test;
@@ -8,11 +10,19 @@ public class XmlDecoderTest {
 
     @Test
     public void decode() throws InvalidUser {
-        /*
-         * XmlDecoder decoder = new XmlDecoder(); User data = new
-         * User("ABC123"); String user =
-         * "<user xmlns=\"urn:scim:schemas:core:1.0\"><id>005D0000001Az1u</id><userName>bjensen@example.com</userName></user>"
-         * ; decoder.decode(user, data);
-         */
+
+        XmlDecoder decoder = new XmlDecoder();
+        User data = new User("ABC123");
+        String user = "<urn:User xmlns:urn=\"urn:scim:schemas:core:1.0\"><id>ABCDE-12345-EFGHI-78910</id><userName>kaan</userName><externalId>djfkhasdjkfha</externalId><name><familyName>Andersson</familyName><givenName>Karl</givenName></name><displayName>Kalle</displayName><nickName>Kalle Anka</nickName><profileUrl>https://example.com</profileUrl><title>master</title><userType>super</userType><preferredLanguage>swedish</preferredLanguage><locale>sv</locale><password>kan123!</password><emails><email><value>karl@andersson.se</value><primary>false</primary><type>home</type></email><email><value>karl.andersson@work.com</value><primary>true</primary><type>work</type></email></emails></urn:User>";
+        decoder.decode(user, data);
+
+        
+        Assert.assertEquals("ABCDE-12345-EFGHI-78910", data.getId());
+        Assert.assertEquals("kaan", data.getUserName());
+        Assert.assertEquals("Andersson", data.getName().getFamilyName());
+        Assert.assertEquals("karl@andersson.se", data.getEmails().get(0).getValue());
+        
+        
+        x0.scimSchemasCore1.User.Factory.newInstance().newCursor().toEndToken();
     }
 }
--- trunk/ScimCore/src/test/java/info/simplecloud/core/handlers/PluralHandlerTest.java
+++ trunk/ScimCore/src/test/java/info/simplecloud/core/handlers/PluralHandlerTest.java
@@ -8,6 +8,8 @@ import info.simplecloud.core.types.PluralType;
 import java.util.ArrayList;
 import java.util.List;
 
+import junit.framework.Assert;
+
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -39,9 +41,9 @@ public class PluralHandlerTest {
         List<PluralType<String>> result = (List<PluralType<String>>) ph.decode(indata, null, new MetaData(StringHandler.class,
                 DummyType.class, "email"));
 
-        result.contains(new PluralType<String>("item1", null, false, false));
-        result.contains(new PluralType<String>("item2", null, false, false));
-        result.contains(new PluralType<String>("item3", null, false, false));
+        Assert.assertTrue(result.contains(new PluralType<String>("item1", null, false, false)));
+        Assert.assertTrue(result.contains(new PluralType<String>("item2", null, false, false)));
+        Assert.assertTrue(result.contains(new PluralType<String>("item3", null, false, false)));
     }
 
     @Test(expected = InvalidUser.class)
@@ -62,7 +64,6 @@ public class PluralHandlerTest {
         indata.put(item2);
         indata.put(item3);
         ph.decode(indata, null, new MetaData(StringHandler.class, DummyType.class, "email"));
-
     }
 
     @Test
@@ -76,7 +77,7 @@ public class PluralHandlerTest {
 
         for (int i = 0; i < result.length(); i++) {
             String value = result.getJSONObject(i).getString("value");
-            indata.contains(new PluralType<String>(value, null, false, false));
+            Assert.assertTrue(indata.contains(new PluralType<String>(value, null, false, false)));
         }
     }
 
@@ -99,13 +100,85 @@ public class PluralHandlerTest {
         List<PluralType<String>> result = (List<PluralType<String>>) ph.decodeXml(emails, null, new MetaData(StringHandler.class,
                 DummyType.class, "email"));
 
-        result.contains(new PluralType<String>("email1", null, false, false));
-        result.contains(new PluralType<String>("email2", null, false, false));
-        result.contains(new PluralType<String>("email3", null, false, false));
+        Assert.assertTrue(result.contains(new PluralType<String>("email1", null, false, false)));
+        Assert.assertTrue(result.contains(new PluralType<String>("email2", null, false, false)));
+        Assert.assertTrue(result.contains(new PluralType<String>("email3", null, false, false)));
+    }
+
+    @Test(expected = InvalidUser.class)
+    public void decodeXmlMultiplePrimary() throws JSONException, InvalidUser {
+        Emails emails = Emails.Factory.newInstance();
+        PluralAttribute email1 = emails.addNewEmail();
+        email1.setPrimary(false);
+        email1.setValue("email1");
+        email1.setType("home");
+        PluralAttribute email2 = emails.addNewEmail();
+        email2.setPrimary(true);
+        email2.setValue("email2");
+        email2.setType("work");
+        PluralAttribute email3 = emails.addNewEmail();
+        email3.setPrimary(true);
+        email3.setValue("email3");
+        email3.setType("bonus");
+
+        ph.decodeXml(emails, null, new MetaData(StringHandler.class, DummyType.class, "email"));
     }
 
     @Test
     public void encodeXml() throws JSONException {
-        // TODO implement test
+        List<PluralType<String>> indata = new ArrayList<PluralType<String>>();
+        indata.add(new PluralType<String>("email1", "work", false, false));
+        indata.add(new PluralType<String>("email2", "home", false, false));
+        indata.add(new PluralType<String>("email3", "bonus", false, false));
+        Emails emails = Emails.Factory.newInstance();
+
+        emails = (Emails) ph.encodeXml(indata, null, new MetaData(StringHandler.class, DummyType.class, "email"), emails);
+
+        for (PluralAttribute singular : emails.getEmailArray()) {
+            Assert.assertTrue(indata.contains(new PluralType<String>(singular.getValue(), null, false, false)));
+        }
+    }
+
+    @Test
+    public void merge() {
+
+        List<PluralType<String>> from = new ArrayList<PluralType<String>>();
+        from.add(new PluralType<String>("email1", "work", false, false));
+        from.add(new PluralType<String>("email2", "home", false, false));
+        from.add(new PluralType<String>("email3", "bonus", false, false));
+        List<PluralType<String>> to = new ArrayList<PluralType<String>>();
+        to = (List<PluralType<String>>) ph.merge(from, to);
+        Assert.assertEquals(from.size(), to.size());
+        Assert.assertEquals(from, to);
+
+        from = new ArrayList<PluralType<String>>();
+        from.add(new PluralType<String>("email3", "bonus", false, true));
+        to = (List<PluralType<String>>) ph.merge(from, to);
+        Assert.assertEquals(2, to.size());
+        Assert.assertTrue(to.contains(new PluralType<String>("email2", "home", false, false)));
+        Assert.assertTrue(to.contains(new PluralType<String>("email1", "work", false, false)));
+
+        from = new ArrayList<PluralType<String>>();
+        from.add(new PluralType<String>("email3", "bonus", true, false));
+        to.get(0).setPrimary(true);
+        to = (List<PluralType<String>>) ph.merge(from, to);
+        int primaries = 0;
+        for (PluralType<String> singular : to) {
+            if (singular.isPrimary()) {
+                primaries++;
+                Assert.assertEquals(new PluralType<String>("email3", "bonus", true, false), singular);
+            }
+        }
+        Assert.assertEquals(1, primaries);
+
+        from = new ArrayList<PluralType<String>>();
+        from.add(new PluralType<String>("email4", "home", false, false));
+        from.add(new PluralType<String>("email5", "bonus", false, false));
+        to = (List<PluralType<String>>) ph.merge(from, to);
+        Assert.assertTrue(to.contains(new PluralType<String>("email1", null, false, false)));
+        Assert.assertTrue(to.contains(new PluralType<String>("email2", null, false, false)));
+        Assert.assertTrue(to.contains(new PluralType<String>("email3", null, false, false)));
+        Assert.assertTrue(to.contains(new PluralType<String>("email4", null, false, false)));
+        Assert.assertTrue(to.contains(new PluralType<String>("email5", null, false, false)));
     }
 }
