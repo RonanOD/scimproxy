906d7d855ac97c77b2ea8ece2223c6d4e4c2a72a
erik@wahlstromstekniska.se <erik@wahlstromstekniska.se@34e1432f-1fb2-72fa-2d63-1a37ba64b630>
2011-09-09 18:08:36 +0000

    Adding toggling of JSON and XML format in list.

--- scimproxy/src/main/java/info/simplecloud/scimproxy/viewer/Edit.java
+++ scimproxy/src/main/java/info/simplecloud/scimproxy/viewer/Edit.java
@@ -16,6 +16,7 @@ import org.apache.commons.httpclient.auth.AuthScope;
 import org.apache.commons.httpclient.methods.PostMethod;
 import org.apache.commons.httpclient.methods.PutMethod;
 import org.apache.commons.httpclient.params.HttpMethodParams;
+import org.apache.commons.lang.StringEscapeUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
@@ -51,8 +52,10 @@ public class Edit extends HttpServlet {
 	        resp.getWriter().println("<head>");
 	        resp.getWriter().println("<script type=\"text/javascript\" src=\"/js/jquery-1.6.2.min.js\"></script>");
 	        resp.getWriter().println("<link rel=\"stylesheet\" type=\"text/css\" href=\"/css/style.css\" />");
+	        resp.getWriter().println("<link href=\"/css/prettify.css\" type=\"text/css\" rel=\"stylesheet\" />");
+	        resp.getWriter().println("<script type=\"text/javascript\" src=\"/js/prettify/prettify.js\"></script>");
 	        resp.getWriter().println("</head>");
-	        resp.getWriter().println("<body>");
+	        resp.getWriter().println("<body onload=\"prettyPrint()\">");
 
 	        resp.getWriter().println("<div id=\"container\">");
 	        resp.getWriter().println("<div id=\"header\"><h1>Simple Cloud Identity Management</h1></div>"); 
@@ -94,7 +97,7 @@ public class Edit extends HttpServlet {
 					if(responseCode == 200) {
 				        resp.getWriter().print("Resource replaced successfully.<br/><br/>");
 				        resp.getWriter().print("<b>Response header:</b>");
-				        resp.getWriter().print("<pre>");
+				        resp.getWriter().print("<pre class=\"prettyprint\">");
 				        
 				        for(int i=0; i<method.getResponseHeaders().length; i++) {
 					        resp.getWriter().print("<br/>" + method.getResponseHeaders()[i].getName() + ": " + method.getResponseHeaders()[i].getValue());
@@ -102,8 +105,13 @@ public class Edit extends HttpServlet {
 				        
 				        resp.getWriter().print("</pre>");
 				        resp.getWriter().print("<b>Response body:</b><br/>");
-				        resp.getWriter().print("<pre>");
-				        resp.getWriter().print(method.getResponseBodyAsString());
+				        resp.getWriter().print("<pre class=\"prettyprint\">");
+				        if("application/json".equals(encoding)) {
+				        	resp.getWriter().print(method.getResponseBodyAsString());
+				        }
+				        else {
+				        	resp.getWriter().print(StringEscapeUtils.escapeHtml(method.getResponseBodyAsString()));
+				        }
 				        resp.getWriter().print("</pre>");
 					}
 					else {
