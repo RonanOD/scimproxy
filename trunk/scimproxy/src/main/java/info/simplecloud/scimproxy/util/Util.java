fabe0eca6f6089e264a2427a37464eb94e168c1f
erik@wahlstromstekniska.se <erik@wahlstromstekniska.se@34e1432f-1fb2-72fa-2d63-1a37ba64b630>
2011-09-11 08:28:50 +0000

    Adding json pretty print on output.

--- trunk/scimproxy/src/main/java/info/simplecloud/scimproxy/util/Util.java
+++ trunk/scimproxy/src/main/java/info/simplecloud/scimproxy/util/Util.java
@@ -113,6 +113,84 @@ public class Util {
         return attributesList;
     }
     
+    
+
+    private static String repeat(String s, int count) {
+    	String str = "";
+    	for(int i=0; i<count; i++) {
+    		str += s;
+    	}
+        return str;
+    }
+    
+    // Transformed into java from https://github.com/umbrae/jsonlintdotcom/blob/master/c/js/jsl.format.js
+    public static String formatJsonPretty(String json) {
+
+        int i           = 0;
+        int il          = 0;
+        String tab         = "    ";
+        String newJson     = "";
+        int indentLevel = 0;
+        boolean inString    = false;
+        char currentChar;
+
+        for (i = 0, il = json.length(); i < il; i += 1) { 
+            currentChar = json.charAt(i);
+
+            switch (currentChar) {
+            case '{': 
+            case '[': 
+                if (!inString) { 
+                    newJson += currentChar + "\n" + repeat(tab, indentLevel + 1);
+                    indentLevel += 1; 
+                } else { 
+                    newJson += currentChar; 
+                }
+                break; 
+            case '}': 
+            case ']': 
+                if (!inString) { 
+                    indentLevel -= 1; 
+                    newJson += "\n" + repeat(tab, indentLevel) + currentChar; 
+                } else { 
+                    newJson += currentChar; 
+                } 
+                break; 
+            case ',': 
+                if (!inString) { 
+                    newJson += ",\n" + repeat(tab, indentLevel); 
+                } else { 
+                    newJson += currentChar; 
+                } 
+                break; 
+            case ':': 
+                if (!inString) { 
+                    newJson += ": "; 
+                } else { 
+                    newJson += currentChar; 
+                } 
+                break; 
+            case ' ':
+            case '\n':
+            case '\t':
+                if (inString) {
+                    newJson += currentChar;
+                }
+                break;
+            case '"': 
+                if (i > 0 && json.charAt(i - 1) != '\\') {
+                    inString = !inString; 
+                }
+                newJson += currentChar; 
+                break;
+            default: 
+                newJson += currentChar; 
+                break;                    
+            } 
+        } 
+
+        return newJson; 
+    }
 	
 	
 }
