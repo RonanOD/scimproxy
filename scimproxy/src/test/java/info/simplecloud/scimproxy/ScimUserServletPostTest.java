cd900094cef4d0b41ebeca1aa24df715fc39f805
samuel@erdtman.se <samuel@erdtman.se@34e1432f-1fb2-72fa-2d63-1a37ba64b630>
2011-10-08 15:01:07 +0000

    Beginning of new viewer

--- scimproxy/src/test/java/info/simplecloud/scimproxy/ScimUserServletPostTest.java
+++ scimproxy/src/test/java/info/simplecloud/scimproxy/ScimUserServletPostTest.java
@@ -1,13 +1,6 @@
 package info.simplecloud.scimproxy;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import info.simplecloud.core.Resource;
 import info.simplecloud.core.User;
-import info.simplecloud.core.extensions.EnterpriseAttributes;
-import info.simplecloud.core.extensions.types.Manager;
-import info.simplecloud.core.types.PluralType;
 
 import org.junit.Assert;
 import org.junit.BeforeClass;
@@ -49,7 +42,6 @@ public class ScimUserServletPostTest {
         Assert.assertEquals(201, response.getStatus());
     }
 
-    /*
     @Test
     public void createUserXml() throws Exception {
 
@@ -69,5 +61,5 @@ public class ScimUserServletPostTest {
 
         Assert.assertEquals(201, response.getStatus());
     }
-*/
+
 }
