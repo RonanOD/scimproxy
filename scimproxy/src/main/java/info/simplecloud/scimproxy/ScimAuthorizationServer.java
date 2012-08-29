ed13291a95020e3b6732488da4163d9f1359576c
samuel@erdtman.se <samuel@erdtman.se@34e1432f-1fb2-72fa-2d63-1a37ba64b630>
2012-08-29 07:00:27 +0000

    corrected oauth compatebilety

--- scimproxy/src/main/java/info/simplecloud/scimproxy/ScimAuthorizationServer.java
+++ scimproxy/src/main/java/info/simplecloud/scimproxy/ScimAuthorizationServer.java
@@ -5,9 +5,7 @@ import info.simplecloud.scimproxy.authentication.AuthenticationUsers;
 import info.simplecloud.scimproxy.authentication.Basic;
 import info.simplecloud.scimproxy.config.Config;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.net.URLDecoder;
 import java.security.SecureRandom;
 import java.util.Random;
 
@@ -68,38 +66,32 @@ public class ScimAuthorizationServer extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        
+
         String code = req.getParameter("code");
-        String grant_type = req.getParameter("grant_type");
-        String redirect_uri = req.getParameter("redirect_uri");
-        
-        
+        //String grant_type = req.getParameter("grant_type");
+        //String redirect_uri = req.getParameter("redirect_uri");
+
         // Access Token Request
         // grant_type=authorization_code&code=SplxlOBeZQQYbYS6WxSbIA&redirect_uri=https%3A%2F%2Fclient%2Eexample%2Ecom%2Fcb
         Config.getInstance();
-        
+
         AuthenticateUser user = null;
         if (code != null) {
             user = AuthenticationUsers.getInstance().getUserWithCode(code);
         } else {
-            String basicAuth = req.getHeader("Authorization");
-            if(basicAuth == null) {
-                System.out.println("Simulating basic auth");
-                String username = req.getParameter("username");
-                String password = req.getParameter("password");
-                basicAuth = "Basic " + Base64.encodeBase64String((username + ":" + password).getBytes());
-            }    
-            
+            String username = req.getParameter("username");
+            String password = req.getParameter("password");
+            String basicAuth = "Basic " + Base64.encodeBase64String((username + ":" + password).getBytes());
+
             System.out.println("basicAuth: " + basicAuth);
             Basic basic = new Basic();
             if (!basic.authenticate(basicAuth)) {
-                resp.setHeader("WWW-authenticate", "basic  realm='scimproxy'");
-                resp.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Authenticate.");
+                resp.sendError(HttpServletResponse.SC_UNAUTHORIZED);
                 return;
             }
             user = basic.getAuthUser();
         }
-        
+
         if (user == null) {
             resp.sendError(HttpServletResponse.SC_UNAUTHORIZED);
             return;
@@ -115,7 +107,6 @@ public class ScimAuthorizationServer extends HttpServlet {
         byte[] tokenBytes = new byte[20];
         random.nextBytes(tokenBytes);
 
-        
         String accessToken = new String(Hex.encodeHex(tokenBytes));
         user.setAccessToken(accessToken);
         resp.setHeader("Content-Type", "application/json");
